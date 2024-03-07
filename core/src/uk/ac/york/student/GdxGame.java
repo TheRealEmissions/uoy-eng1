9d28e9ac0966f86e67fa644bc4ad105a88d2d253
TheRealEmissions <me@liammartin.site>
2024-03-07 10:52:27 +0000

    Remove default sprite and textures

--- core/src/uk/ac/york/student/GdxGame.java
+++ core/src/uk/ac/york/student/GdxGame.java
@@ -6,26 +6,17 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class GdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
 	
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	public void create() {
 	}
 
 	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void render() {
+		ScreenUtils.clear(0, 0, 0, 1);
 	}
 	
 	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void dispose() {
 	}
 }
