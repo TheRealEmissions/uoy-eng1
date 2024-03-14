ae4756613b3ea0933839989764753dced5b15d3e
TheRealEmissions <unitedrealm@hotmail.com>
2024-03-14 16:15:12 +0000

    Added button click to SoundManager

--- core/src/uk/ac/york/student/audio/sound/SoundManager.java
+++ core/src/uk/ac/york/student/audio/sound/SoundManager.java
@@ -2,6 +2,11 @@ package uk.ac.york.student.audio.sound;
 
 import lombok.Getter;
 import uk.ac.york.student.audio.AudioManager;
+import uk.ac.york.student.audio.sound.elements.ButtonClickSound;
+import uk.ac.york.student.settings.GamePreferences;
+import uk.ac.york.student.utils.EnumSupplierMap;
+
+import java.util.function.Supplier;
 
 /**
  * Singleton class that manages the sound for the game
@@ -12,6 +17,13 @@ public class SoundManager implements AudioManager {
      */
     @Getter
     private static final SoundManager instance = new SoundManager();
+
+    private static final Supplier<GameSound> buttonClickSound = ButtonClickSound::new;
+    @Getter
+    private static final EnumSupplierMap<Sounds, GameSound> sounds = new EnumSupplierMap<>(Sounds.class);
+    static {
+        sounds.put(Sounds.BUTTON_CLICK, buttonClickSound);
+    }
 
     /**
      * Private constructor to prevent instantiation
