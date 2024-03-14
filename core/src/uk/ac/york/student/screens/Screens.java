9df78ba17145f2b6df9fb501ad229e603318798c
TheRealEmissions <unitedrealm@hotmail.com>
2024-03-14 13:38:53 +0000

    Added Screens make-shift enum

---
+++ core/src/uk/ac/york/student/screens/Screens.java
@@ -0,0 +1,50 @@
+package uk.ac.york.student.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import lombok.Getter;
+import lombok.experimental.UtilityClass;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+@UtilityClass
+/**
+ * A utility class to store the classes of the different screens loaded with reflection.
+ */
+public final class Screens {
+    public static Class<MainMenuScreen> MAIN_MENU;
+    public static Class<LoadingScreen> LOADING;
+    public static Class<GameScreen> GAME;
+    public static Class<PreferencesScreen> PREFERENCES;
+    public static Class<EndScreen> END;
+
+    static {
+        Field[] fields = Screens.class.getFields();
+        // using reflection, set the fields to their respective classes
+        for (Field field : fields) {
+            String name = field.getName();
+            // convert name to UpperCamelCase
+            name = Arrays.stream(name.split("_"))
+                    .map(s -> s.substring(0, 1).toUpperCase() + s.substring(1).toLowerCase())
+                    .reduce("", String::concat);
+            name += "Screen";
+
+            String path = Screens.class.getPackageName() + "." + name;
+            try {
+                // Many linters will complain about this, but it's fine in this case
+                // as it does not have an effect on the program's security,
+                // performance, and it does not violate encapsulation.
+                //
+                // Although the fields should be final, they are not because
+                // this utility class is set up with reflection and designed to emulate an enum
+                field.set(null, Class.forName(path));
+            } catch (ClassNotFoundException | IllegalAccessException e) {
+                Gdx.app.error("Screens", "Could not find class " + path);
+            }
+        }
+    }
+}
