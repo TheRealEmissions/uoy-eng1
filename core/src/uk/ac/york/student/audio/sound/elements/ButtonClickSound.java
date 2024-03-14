ea9a51986b56cdbe35621a2646b0d5d458824d47
TheRealEmissions <unitedrealm@hotmail.com>
2024-03-14 16:13:04 +0000

    ButtonClickSound.java

---
+++ core/src/uk/ac/york/student/audio/sound/elements/ButtonClickSound.java
@@ -0,0 +1,38 @@
+package uk.ac.york.student.audio.sound.elements;
+
+import uk.ac.york.student.audio.sound.GameSound;
+import uk.ac.york.student.settings.GamePreferences;
+
+public class ButtonClickSound extends GameSound {
+    /**
+     * Creates a new GameSound with the default path
+     */
+    public ButtonClickSound() {
+        super("audio/sounds/mixkit-classic-click.mp3");
+    }
+
+    @Override
+    public long play() {
+        GamePreferences.SoundPreferences soundPreferences = (GamePreferences.SoundPreferences) GamePreferences.SOUND.getPreference();
+        if (soundPreferences.isEnabled()) {
+            return super.play();
+        } else {
+            return -1;
+        }
+    }
+
+    @Override
+    public void stop(long soundId) {
+        sound.stop(soundId);
+    }
+
+    @Override
+    public void pause(long soundId) {
+        sound.pause(soundId);
+    }
+
+    @Override
+    public void resume(long soundId) {
+        sound.resume(soundId);
+    }
+}
