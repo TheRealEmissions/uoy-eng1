e70b2cbaf7b94dadd7dd635bce376a0c5cf53f06
TheRealEmissions <unitedrealm@hotmail.com>
2024-03-14 17:47:00 +0000

    Fixed memory leaks

--- core/src/uk/ac/york/student/screens/PreferencesScreen.java
+++ core/src/uk/ac/york/student/screens/PreferencesScreen.java
@@ -2,6 +2,8 @@ package uk.ac.york.student.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.Touchable;
@@ -13,12 +15,19 @@ import uk.ac.york.student.GdxGame;
 import uk.ac.york.student.assets.skins.SkinManager;
 import uk.ac.york.student.assets.skins.Skins;
 import uk.ac.york.student.audio.music.MusicManager;
+import uk.ac.york.student.audio.sound.GameSound;
+import uk.ac.york.student.audio.sound.SoundManager;
+import uk.ac.york.student.audio.sound.Sounds;
 import uk.ac.york.student.settings.GamePreferences;
 
 import java.util.function.Supplier;
 
 public class PreferencesScreen extends GenericScreen {
     private final Stage processor;
+    private final GameSound buttonClick = SoundManager.getSupplierSounds().getResult(Sounds.BUTTON_CLICK);
+    private final Skin craftacularSkin = SkinManager.getSkins().getResult(Skins.CRAFTACULAR);
+    private final Texture stoneWallTexture = new Texture(Gdx.files.internal("images/StoneWall.png"));
+    private final Texture bottomUpBlackGradient = new Texture(Gdx.files.internal("images/BottomUpBlackGradient.png"));
     private enum Labels {
         BACK_BUTTON("Head Back"),
         TITLE("Settings"),
@@ -53,10 +62,11 @@ public class PreferencesScreen extends GenericScreen {
 
     @Override
     public void show() {
-        final TextButton musicToggleButton = new TextButton(Labels.MUSIC_ENABLED.getLabel(((GamePreferences.MusicPreferences) GamePreferences.MUSIC.getPreference()).isEnabled() ? "ON" : "OFF"), SkinManager.getSkins().getResult(Skins.CRAFTACULAR));
+        final TextButton musicToggleButton = new TextButton(Labels.MUSIC_ENABLED.getLabel(((GamePreferences.MusicPreferences) GamePreferences.MUSIC.getPreference()).isEnabled() ? "ON" : "OFF"), craftacularSkin);
         musicToggleButton.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
+                buttonClick.play();
                 GamePreferences.MusicPreferences preference = (GamePreferences.MusicPreferences) GamePreferences.MUSIC.getPreference();
                 boolean nowEnabled = !preference.isEnabled();
                 preference.setEnabled(nowEnabled);
@@ -69,16 +79,20 @@ public class PreferencesScreen extends GenericScreen {
             }
         });
 
-        final Slider musicVolumeSlider = new Slider(0f, 1f, 0.1f, false, SkinManager.getSkins().getResult(Skins.CRAFTACULAR));
+        final Slider musicVolumeSlider = new Slider(0f, 1f, 0.01f, false, craftacularSkin);
         musicVolumeSlider.setVisualPercent(((GamePreferences.MusicPreferences) GamePreferences.MUSIC.getPreference()).getVolume());
+
+        Label musicVolumeLabel = new Label(Labels.MUSIC_VOLUME.getLabel(Math.round(musicVolumeSlider.getValue() * 100) + "%"), craftacularSkin);
+
         musicVolumeSlider.addListener(event -> {
             GamePreferences.MusicPreferences preference = (GamePreferences.MusicPreferences) GamePreferences.MUSIC.getPreference();
             preference.setVolume(musicVolumeSlider.getValue());
             MusicManager.BACKGROUND_MUSIC.setVolume(musicVolumeSlider.getValue());
+            musicVolumeLabel.setText(Labels.MUSIC_VOLUME.getLabel(Math.round(musicVolumeSlider.getValue() * 100) + "%"));
             return false;
         });
 
-        final TextButton soundToggleButton = new TextButton(Labels.SOUND_ENABLED.getLabel(((GamePreferences.SoundPreferences) GamePreferences.SOUND.getPreference()).isEnabled() ? "ON" : "OFF"), SkinManager.getSkins().getResult(Skins.CRAFTACULAR));
+        final TextButton soundToggleButton = new TextButton(Labels.SOUND_ENABLED.getLabel(((GamePreferences.SoundPreferences) GamePreferences.SOUND.getPreference()).isEnabled() ? "ON" : "OFF"), craftacularSkin);
         soundToggleButton.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
@@ -86,24 +100,27 @@ public class PreferencesScreen extends GenericScreen {
                 boolean nowEnabled = !preference.isEnabled();
                 preference.setEnabled(nowEnabled);
                 soundToggleButton.setText(Labels.SOUND_ENABLED.getLabel(nowEnabled ? "ON" : "OFF"));
+                buttonClick.play();
             }
         });
 
-        final TextButton debugScreenToggleButton = new TextButton(Labels.DEBUG_SCREEN_ENABLED.getLabel(((GamePreferences.DebugScreenPreferences) GamePreferences.DEBUG_SCREEN.getPreference()).isEnabled() ? "ON" : "OFF"), SkinManager.getSkins().getResult(Skins.CRAFTACULAR));
+        final TextButton debugScreenToggleButton = new TextButton(Labels.DEBUG_SCREEN_ENABLED.getLabel(((GamePreferences.DebugScreenPreferences) GamePreferences.DEBUG_SCREEN.getPreference()).isEnabled() ? "ON" : "OFF"), craftacularSkin);
         debugScreenToggleButton.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
+                buttonClick.play();
                 GamePreferences.DebugScreenPreferences preference = (GamePreferences.DebugScreenPreferences) GamePreferences.DEBUG_SCREEN.getPreference();
                 boolean nowEnabled = !preference.isEnabled();
                 preference.setEnabled(nowEnabled);
-                debugScreenToggleButton.setText(Labels.DEBUG_SCREEN_ENABLED.getLabel(nowEnabled ? "ON" : "OFF"));
+                game.setScreen(Screens.PREFERENCES);
             }
         });
 
-        final TextButton backButton = new TextButton(Labels.BACK_BUTTON.getLabel(), SkinManager.getSkins().getResult(Skins.CRAFTACULAR));
+        final TextButton backButton = new TextButton(Labels.BACK_BUTTON.getLabel(), craftacularSkin);
         backButton.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
+                SoundManager.getSounds().get(Sounds.BUTTON_CLICK).play();
                 game.setScreen(Screens.MAIN_MENU);
             }
         });
@@ -113,22 +130,21 @@ public class PreferencesScreen extends GenericScreen {
         if (((GamePreferences.DebugScreenPreferences) GamePreferences.DEBUG_SCREEN.getPreference()).isEnabled()) {
             table.setDebug(true);
         }
-        table.setSkin(SkinManager.getSkins().getResult(Skins.CRAFTACULAR));
+        table.setSkin(craftacularSkin);
         processor.addActor(table);
         Cell<Label> titleCell = table.add(Labels.TITLE.getLabel()).colspan(2).pad(0, 0, 100, 0);
         titleCell.getActor().setFontScale(1.5f);
         table.row();
-        table.add(musicToggleButton).fillX().uniformX().pad(0, 0, 25, 0);
+        table.add(musicToggleButton).fillX().uniformX();
         table.row();
         Stack stack = new Stack();
         stack.add(musicVolumeSlider);
-        Label musicVolumeLabel = new Label(Labels.MUSIC_VOLUME.getLabel(String.valueOf(musicVolumeSlider.getValue())), SkinManager.getSkins().getResult(Skins.CRAFTACULAR));
         // centre the label
         musicVolumeLabel.setAlignment(1);
         // make it so the label is not interactable
         musicVolumeLabel.setTouchable(Touchable.disabled);
         stack.add(musicVolumeLabel);
-        table.add(stack).center().fillX().uniformX();
+        table.add(stack).center().fillX().uniformX().pad(0, 0, 25 ,0);
         table.row();
         table.add(soundToggleButton).fillX().uniformX().pad(0, 0, 25, 0);
         table.row();
@@ -145,6 +161,23 @@ public class PreferencesScreen extends GenericScreen {
     public void render(float v) {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        Batch batch = processor.getBatch();
+        batch.begin();
+
+        // draw in a grid pattern
+        int width = stoneWallTexture.getWidth() / 6;
+        int height = stoneWallTexture.getHeight() / 6;
+        for (int x = 0; x < Gdx.graphics.getWidth(); x += width) {
+            for (int y = 0; y < Gdx.graphics.getHeight(); y += height) {
+                batch.draw(stoneWallTexture, x, y, width, height);
+            }
+        }
+
+        // draw the gradient at the bottom
+        batch.draw(bottomUpBlackGradient, 0, 0, Gdx.graphics.getWidth(), bottomUpBlackGradient.getHeight());
+
+        batch.end();
 
         processor.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
         processor.draw();
@@ -173,5 +206,7 @@ public class PreferencesScreen extends GenericScreen {
     @Override
     public void dispose() {
         processor.dispose();
+        craftacularSkin.dispose();
+        buttonClick.dispose();
     }
 }
