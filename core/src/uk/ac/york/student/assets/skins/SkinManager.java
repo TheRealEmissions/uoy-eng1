4114c257636288a44aedb0728ec5a42bc3404265
TheRealEmissions <unitedrealm@hotmail.com>
2024-03-14 16:15:01 +0000

    SkinManager

--- core/src/uk/ac/york/student/assets/skins/SkinManager.java
+++ core/src/uk/ac/york/student/assets/skins/SkinManager.java
@@ -4,19 +4,19 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import lombok.Getter;
 import lombok.experimental.UtilityClass;
+import uk.ac.york.student.utils.EnumSupplierMap;
 import uk.ac.york.student.utils.SupplierMap;
 
-import java.util.HashMap;
 import java.util.function.Supplier;
 
 @UtilityClass
 public class SkinManager {
-    private static final Supplier<Skin> craftacular = () -> new Skin(Gdx.files.internal("skins/craftacular/craftacular-ui.json"));
+    private static final Supplier<Skin> craftacular = () -> new Skin(Gdx.files.internal("skins/craftacular/skin/craftacular-ui.json"));
 
     @Getter
-    private static final SupplierMap<Skins, Skin> buttons = new SupplierMap<>();
+    private static final EnumSupplierMap<Skins, Skin> skins = new EnumSupplierMap<>(Skins.class);
 
     static {
-        buttons.put(Skins.CRAFTACULAR, craftacular);
+        skins.put(Skins.CRAFTACULAR, craftacular);
     }
 }
