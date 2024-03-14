547c32ee3d00605a0163e80fb9caee2de74bceb0
TheRealEmissions <unitedrealm@hotmail.com>
2024-03-14 19:25:10 +0000

    Added main menu clouds preferences

--- core/src/uk/ac/york/student/settings/GamePreferences.java
+++ core/src/uk/ac/york/student/settings/GamePreferences.java
@@ -3,13 +3,13 @@ package uk.ac.york.student.settings;
 import com.badlogic.gdx.Gdx;
 import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
-import uk.ac.york.student.audio.music.MusicManager;
 
 @Getter
 public enum GamePreferences {
     MUSIC(new MusicPreferences()),
     SOUND(new SoundPreferences()),
-    DEBUG_SCREEN(new DebugScreenPreferences());
+    DEBUG_SCREEN(new DebugScreenPreferences()),
+    MAIN_MENU_CLOUDS(new MainMenuCloudsPreferences());
 
     private final Preference preference;
 
@@ -25,6 +25,42 @@ public enum GamePreferences {
 
     public interface Preference {
         String getKey(String key);
+    }
+
+    public static class MainMenuCloudsPreferences implements Preference {
+        private static final String ENABLED = "enabled";
+        private static final boolean DEFAULT_ENABLED = true;
+        private static final String SPEED = "speed";
+        private static final float DEFAULT_SPEED = 1f;
+
+        public MainMenuCloudsPreferences() {
+            GamePreferences.getPreferences().putBoolean(getKey(ENABLED), DEFAULT_ENABLED);
+            GamePreferences.getPreferences().putFloat(getKey(SPEED), DEFAULT_SPEED);
+            GamePreferences.getPreferences().flush();
+        }
+
+        public boolean isEnabled() {
+            return GamePreferences.getPreferences().getBoolean(getKey(ENABLED));
+        }
+
+        public void setEnabled(boolean b) {
+            GamePreferences.getPreferences().putBoolean(getKey(ENABLED), b);
+            GamePreferences.getPreferences().flush();
+        }
+
+        public float getSpeed() {
+            return GamePreferences.getPreferences().getFloat(getKey(SPEED));
+        }
+
+        public void setSpeed(float speed) {
+            GamePreferences.getPreferences().putFloat(getKey(SPEED), speed);
+            GamePreferences.getPreferences().flush();
+        }
+
+        @Override
+        public String getKey(String key) {
+            return "main_menu_clouds." + key;
+        }
     }
 
     public static class DebugScreenPreferences implements Preference {
@@ -92,9 +128,13 @@ public enum GamePreferences {
     public static class SoundPreferences implements Preference {
         private static final String ENABLED = "enabled";
         private static final boolean DEFAULT_ENABLED = true;
+        private static final String VOLUME = "volume";
+        private static final float DEFAULT_VOLUME = 1f;
+
 
         public SoundPreferences() {
             GamePreferences.getPreferences().putBoolean(getKey(ENABLED), DEFAULT_ENABLED);
+            GamePreferences.getPreferences().putFloat(getKey(VOLUME), DEFAULT_VOLUME);
             GamePreferences.getPreferences().flush();
         }
 
@@ -104,6 +144,15 @@ public enum GamePreferences {
 
         public void setEnabled(boolean b) {
             GamePreferences.getPreferences().putBoolean(getKey(ENABLED), b);
+            GamePreferences.getPreferences().flush();
+        }
+
+        public float getVolume() {
+            return GamePreferences.getPreferences().getFloat(getKey(VOLUME));
+        }
+
+        public void setVolume(float vol) {
+            GamePreferences.getPreferences().putFloat(getKey(VOLUME), vol);
             GamePreferences.getPreferences().flush();
         }
 
