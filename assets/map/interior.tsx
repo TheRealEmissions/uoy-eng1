65de43c88d91530c0ca48ed2ea36c2e7e1d04a59
TheRealEmissions <unitedrealm@hotmail.com>
2024-03-19 17:40:24 +0000

    Fixed missing assets

--- assets/map/interior.tsx
+++ assets/map/interior.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="interior" tilewidth="16" tileheight="16" tilecount="9035" columns="139">
- <image source="../../Downloads/town full/interior/interior.png" width="2224" height="1040"/>
+ <image source="interior/interior.png" width="2224" height="1040"/>
 </tileset>
