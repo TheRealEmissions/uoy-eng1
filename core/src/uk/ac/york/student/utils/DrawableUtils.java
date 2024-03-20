d7e7ed546d22cb3c895a6cb16b15a06c4b5481b7
TheRealEmissions <unitedrealm@hotmail.com>
2024-03-20 01:51:51 +0000

    Added DrawableUtils

---
+++ core/src/uk/ac/york/student/utils/DrawableUtils.java
@@ -0,0 +1,24 @@
+package uk.ac.york.student.utils;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class DrawableUtils {
+    public static Drawable getColouredDrawable(int width, int height, Color color) {
+        Pixmap pixmap = new Pixmap(width, height, Pixmap.Format.RGBA8888);
+        pixmap.setColor(color);
+        pixmap.fill();
+
+        TextureRegionDrawable drawable = new TextureRegionDrawable(new TextureRegion(new Texture(pixmap)));
+
+        pixmap.dispose();
+
+        return drawable;
+    }
+}
